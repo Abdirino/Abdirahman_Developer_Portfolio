1015b87b8610e5814a0d8808e792d1e1ebc835f9
Abdirino <abdirinoahash@gmail.com>
2023-09-26 13:58:34 +0300

    big screen responsiveness fix

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -13,61 +13,63 @@ const Navbar = () => {
 
     return (
         <>
-            <div className='navbar' onClick={(e) => e.stopPropagation}>
+            <div className="Navbar">
+                <div className='navbar' onClick={(e) => e.stopPropagation}>
 
-                <div className="left">
-                    <div className="nav-icon" onClick={handleClick}>
-                        <i className={click ? "fa-regular fa-circle-xmark" : "fa-solid fa-bars-staggered"}></i>
-                    </div>
-                    <NavLink to='/'>
-                        <div className="nav-logo">
-                            <img src={NavIcon} alt="" />
+                    <div className="left">
+                        <div className="nav-icon" onClick={handleClick}>
+                            <i className={click ? "fa-regular fa-circle-xmark" : "fa-solid fa-bars-staggered"}></i>
                         </div>
-                    </NavLink>
-                    <ul className={click ? "nav-items active" : "nav-items"}>
-                        <li>
-                            <NavLink
-                                exact
-                                to="/"
-                                activeClassName="active"
-                                className="nav-links"
-                                onClick={click ? handleClick : null}
-                            >
-                                Home
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink
-                                exact
-                                to="/about"
-                                activeClassName="active"
-                                className="nav-links"
-                                onClick={click ? handleClick : null}
-                            >
-                                About
-                            </NavLink>
-                        </li>
-                        <li className="nav-links">
-                            <NavLink
-                                exact
-                                to="/services"
-                                activeClassName="active"
-                                className="nav-links"
-                                onClick={click ? handleClick : null}
-                            >
-                                Services
-                            </NavLink>
-                        </li>
-                        <li className="nav-links">Portfolio</li>
-                        <li className="nav-links">Contact</li>
-                    </ul>
-                </div>
+                        <NavLink to='/'>
+                            <div className="nav-logo">
+                                <img src={NavIcon} alt="" />
+                            </div>
+                        </NavLink>
+                        <ul className={click ? "nav-items active" : "nav-items"}>
+                            <li>
+                                <NavLink
+                                    exact
+                                    to="/"
+                                    activeClassName="active"
+                                    className="nav-links"
+                                    onClick={click ? handleClick : null}
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    exact
+                                    to="/about"
+                                    activeClassName="active"
+                                    className="nav-links"
+                                    onClick={click ? handleClick : null}
+                                >
+                                    About
+                                </NavLink>
+                            </li>
+                            <li className="nav-links">
+                                <NavLink
+                                    exact
+                                    to="/services"
+                                    activeClassName="active"
+                                    className="nav-links"
+                                    onClick={click ? handleClick : null}
+                                >
+                                    Services
+                                </NavLink>
+                            </li>
+                            <li className="nav-links">Portfolio</li>
+                            <li className="nav-links">Contact</li>
+                        </ul>
+                    </div>
 
-                <div className="right">
-                    <button className="button">
-                        <span>HIRE ME</span>
-                        <i class="fa-solid fa-angles-right"></i>
-                    </button>
+                    <div className="right">
+                        <button className="button">
+                            <span>HIRE ME</span>
+                            <i class="fa-solid fa-angles-right"></i>
+                        </button>
+                    </div>
                 </div>
             </div>
         </>
